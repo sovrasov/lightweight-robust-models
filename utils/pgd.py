97b5b1647110c3b36cd6ce20e7a1383e56dff07d
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2020-09-22 15:01:38 +0300

    Use euclidean dist for attacks

--- utils/pgd.py
+++ utils/pgd.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 def pgd_attack(model,
@@ -11,6 +12,7 @@ def pgd_attack(model,
                   clip_min=0.0,
                   num_steps=3,
                   step_size=0.01,
+                  euclidean=True,
                   print_process=False):
     #out = model(X)
     #err = (out.data.max(1)[1] != y.data).float().sum()
@@ -35,7 +37,11 @@ def pgd_attack(model,
         eta = step_size * X_pgd.grad.data.sign()
 
         X_pgd = X_pgd + eta
-        eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
+        if euclidean:
+            #eta = epsilon * F.normalize((X_pgd.data - X.data).view(X.size(0), -1)).view(*X.shape)
+            eta = (X_pgd.data - X.data).renorm(p=2, dim=0, maxnorm=epsilon)
+        else:
+            eta = torch.clamp(X_pgd.data - X.data, -epsilon, epsilon)
 
         X_pgd = X.data + eta
         X_pgd = torch.clamp(X_pgd, clip_min, clip_max)
